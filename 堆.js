75113ce341b2a7b0d97411089854aa13cc874c40
darKleaf <2670583526@qq.com>
2023-01-29 11:50:25 +0800

    堆 节流 防抖

--- 堆.js
+++ 堆.js
@@ -24,3 +24,70 @@ function addHeap(k, arr, length) {
   }
   arr[k] = tmp;
 }
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  getParentIndex(i) {
+    return Math.floor((i - 1) / 2);
+  }
+
+  getLeftIndex(i) {
+    return 2 * i + 1;
+  }
+
+  getRightIndex(i) {
+    return 2 * i + 2;
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.shiftUp(this.heap.length - 1);
+  }
+
+  shiftUp(index) {
+    if (index === 0) {
+      return;
+    }
+    const parentIndex = this.getParentIndex(index);
+    if (this.heap[parentIndex] > this.heap[index]) {
+      this.swap(parentIndex, index);
+      this.shiftUp(parentIndex);
+    }
+  }
+
+  swap(i1, i2) {
+    const temp = this.heap[i1];
+    this.heap[i1] = this.heap[i2];
+    this.heap[i2] = temp;
+  }
+
+  pop() {
+    this.heap[0] = this.heap.pop();
+    this.shiftDown(0);
+    return this.heap[0];
+  }
+
+  shiftDown(index) {
+    const leftIndex = this.getLeftIndex(index);
+    const rightIndex = this.getRightIndex(index);
+    if (this.heap[leftIndex] < this.heap[index]) {
+      this.swap(leftIndex, index);
+      this.shiftDown(leftIndex);
+    }
+    if (this.heap[rightIndex] < this.heap[index]) {
+      this.swap(rightIndex, index);
+      this.shiftDown(rightIndex);
+    }
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+}
--- 节流防抖.js
+++ 节流防抖.js
@@ -1,16 +1,81 @@
-// 节流
+// 节流：n秒内只运行一次，若在n秒内重复触发，只有一次生效
+
+function throttled1(fn, delay) {
+  let timer = null;
+  return function _debounce(...arg) {
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn.apply(this, arg);
+        timer = null;
+      }, delay);
+    }
+  };
+}
+
+function throttled2(fn, delay) {
+  let oldTime = Date.now();
+  return function _debounce(...arg) {
+    const newTime = Date.now();
+    if (newTime - oldTime >= delay) {
+      fn.apply(this, arg);
+      oldTime = Date.now();
+    }
+  };
+}
+
+function throttled(fn, delay) {
+  let timer = null;
+  let oldTime = Date.now();
+  return function _throttled(...arg) {
+    const newTime = Date.now();
+    const remainTime = delay - (newTime - oldTime);
+    clearTimeout(timer);
+    if (remainTime <= 0) {
+      fn.apply(this, arg);
+      oldTime = Date.now();
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(this, arg);
+        oldTime = Date.now();
+      }, delay);
+    }
+  };
+}
 
 /**
- * 防抖：在一定时间间隔，多次触发只有一次触发发生
- * 场景：搜索框输入查询，如果用户一直在输入中，没有必要不停地调用接口，等用户停止输入的时候，在调用，设置一个合适的时间间隔
+ * 防抖：n秒后执行该事件，若在n秒内重复触发，则重新计时
  */
 
 function debounce(fn, delay) {
   let timer = null;
-  return function _debounce(...arg) {
+  return function _debounce(...args) {
     if (timer) {
       clearTimeout(timer);
     }
-    timer = setTimeout(fn, delay);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
   };
+}
+
+function debounce1(fn, delay, immediate) {
+  let timer = null;
+  return function _debounce(...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    if (immediate) {
+      const flag = !timer;
+      timer = setTimeout(() => {
+        timer = null;
+      }, delay);
+      if (flag) {
+        fn.apply(this, args);
+      }
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+      }, delay)
+    }
+  }
 }
